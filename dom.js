be49de62659597d3db228462293b53bc7a91b6f7
waspm4n <osmanpulic98@gmail.com>
2023-02-17 14:42:11 +0100

    DOM Crash C 1

--- dom.js
+++ dom.js
@@ -22,9 +22,40 @@
 
 // GET ELEMENTS BY CLASS NAME //
 
-let items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'gray';
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'gray';
+
+// //Daje gresku
+// //items[1].style.backgroundColor = "f4f4f4";
+// // for (let i = 0; i < items.length; i++){
+// //     items[i].style.backgroundColor = "darkblue"
+// // };
+
+// // QUERY SELEKTOR //
+// let header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ffff';
+// let input = document.querySelector('input');
+// input.value = 'Hello world';
+// let submit = document.querySelector('input[type = "Submit"]');
+// submit.value = "SEND";
+// let item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+// let lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral';
+
+// // QUERY SELEKTOR "ALL"//
+// let titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// let even = document.querySelectorAll('li:nth-child(even)')
+// for (let i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = "#f4f4f4";
+//     even[i].style.backgroundColor = '#ccc';
+// };
